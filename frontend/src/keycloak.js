2e1b9171564047f5bcf41638c55f7671b2c3b2a6
Vanina Correa <vanina.correa@usach.cl>
2022-11-15 19:17:59 -0300

    [feat] implementación login keycloak

--- frontend/src/keycloak.js
+++ frontend/src/keycloak.js
@@ -1,14 +1,24 @@
 import Keycloak from "keycloak-js";
 
 const keycloakConfig = {
-    url: 'http://localhost:8082',
-    realm: 'mueblesstgo',
-    clientId: 'springboot-keycloak',
-    onLoad: 'check-sso'
+  url: "http://localhost:8082",
+  realm: "mueblesstgo",
+  clientId: "springboot-keycloak",
+  onLoad: "login-required",
+};
+
+const keycloak = new Keycloak(keycloakConfig);
+
+keycloak.init({ onLoad: keycloakConfig.onLoad }).then((authenticated) => {
+  if (authenticated) {
+    localStorage.setItem("react-token", keycloak.token);
+    console.log(keycloak.token);
+    localStorage.setItem("react-refresh-token", keycloak.refreshToken);
+    console.log(keycloak.refreshToken);
+    localStorage.setItem("react-id-token", keycloak.idToken);
+  } else {
+    keycloak.login();
   }
-  
-  const keycloak = new Keycloak(keycloakConfig)
-
-
+});
 
 export default keycloak;
