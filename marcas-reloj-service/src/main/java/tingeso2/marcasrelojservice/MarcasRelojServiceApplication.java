324f93b960ce1e23b912c06127bd34f75f388abb
Vanina Correa <vanina.correa@usach.cl>
2022-11-19 21:51:09 -0300

    [fix] application marcas de reloj duplicado

--- marcas-reloj-service/src/main/java/tingeso2/marcasrelojservice/MarcasRelojServiceApplication.java
+++ marcas-reloj-service/src/main/java/tingeso2/marcasrelojservice/MarcasRelojServiceApplication.java
@@ -2,10 +2,15 @@ package tingeso2.marcasrelojservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import tingeso2.marcasrelojservice.services.FileStorageProperties;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class MarcasRelojServiceApplication {
 
 	public static void main(String[] args) {
