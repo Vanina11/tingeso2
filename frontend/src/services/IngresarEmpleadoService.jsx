6245f08f3e285185d8d4c23a807cd1477618428a
Vanina Correa <vanina.correa@usach.cl>
2022-11-21 12:24:57 -0300

    [fix] le había sacado el token a ingresar empleados

--- frontend/src/services/IngresarEmpleadoService.jsx
+++ frontend/src/services/IngresarEmpleadoService.jsx
@@ -4,13 +4,14 @@ const url = "http://localhost:8080/empleado";
 const token = localStorage.getItem("token");
 
 class IngresarEmpleadoService {
-    /*
+    
     ingresarEmpleado(empleado) {
         return axios.post(url, empleado, {headers: {Authorization: `Bearer ${token}`}});
-    }*/
+    }
+    /*
     ingresarEmpleado(empleado) {
         return axios.post(url, empleado);
-    }
+    }*/
 }
 
 export default new IngresarEmpleadoService();
