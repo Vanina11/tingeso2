40fb66369b531e180534af5f48fa00d96bf2bbd8
Vanina Correa <vanina.correa@usach.cl>
2022-11-21 17:13:31 -0300

    [fix]

--- frontend/src/services/IngresarEmpleadoService.jsx
+++ frontend/src/services/IngresarEmpleadoService.jsx
@@ -1,17 +1,12 @@
 import axios from "axios";
 
 const url = "http://localhost:8080/empleado";
-const token = localStorage.getItem("token");
 
 class IngresarEmpleadoService {
     
     ingresarEmpleado(empleado) {
-        return axios.post(url, empleado, {headers: {Authorization: `Bearer ${token}`}});
-    }
-    /*
-    ingresarEmpleado(empleado) {
         return axios.post(url, empleado);
-    }*/
+    }
 }
 
 export default new IngresarEmpleadoService();
